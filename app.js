1ababd8a1edbeda39d3f2c865d19b09b4847445d
Tarjei <trylvis@outlook.com>
2018-05-26 18:02:51 +0000

    Initial deploy commit

---
+++ app.js
@@ -0,0 +1,60 @@
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    User          = require("./models/user"),
+    mongoose      = require("mongoose"),
+    flash         = require("connect-flash"),
+    passport      = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground    = require("./models/campground"),
+    Comment       = require("./models/comment"),
+    User          = require("./models/user"),
+    seedDB        = require("./seeds");
+
+// Require routes  
+var commentRoutes    = require("./routes/comment"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+
+mongoose.connect("mongodb://localhost/yelp_camp_v11");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"))
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Middleware to provide currentUser status to all routes
+// Also provide flash messages globally
+app.use(function(req, res, next){
+   //Gets logged in user if any
+   res.locals.currentUser = req.user;
+   //flash config
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
+});
+
+//Using routes
+app.use(indexRoutes);
+app.use(campgroundRoutes);
+app.use(commentRoutes);
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The YelpCamp Server Has Started!");
+});
---
+++ routes/campgrounds.js
@@ -0,0 +1,102 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+var middleware = require("../middleware");
+
+
+
+router.get("/campgrounds", function(req, res){
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
+        }
+    });
+
+});
+
+//CREATE - add new campground to DB
+router.post("/campgrounds", middleware.isLoggedIn, function(req, res){
+   // get data from form and add to campgrouns array
+   var name = req.body.name;
+   var price = req.body.price;
+   var image = req.body.image;
+   var desc = req.body.description;
+   var author = {
+       id: req.user._id,
+       username: req.user.username
+   };
+   var newCampground = {name: name, price: price, image: image, description: desc, author: author};
+   // Create a new campground and save to DB
+   Campground.create(newCampground, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       } else {
+            //Log new campground
+            console.log("** Created campground: **")
+            console.log(newlyCreated);
+            // redirect back to campgrounds page
+            res.redirect("/campgrounds");
+       }
+   });
+
+});
+
+//NEW - show form to create new campground
+router.get("/campgrounds/new", middleware.isLoggedIn, function(req, res){
+   res.render("campgrounds/new"); 
+});
+
+// SHOW - shows more info about one campground
+router.get("/campgrounds/:id", function(req, res){
+    //find the campground with the provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else {
+            //render show template with that campground
+             res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+// EDIT CAMPGROUND ROUTE
+router.get("/campgrounds/:id/edit", middleware.checkCampgroundOwnership, function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            req.flash("error", "Campground not found, please try again")
+        }
+        res.render("campgrounds/edit", {campground: foundCampground});
+    });
+});
+
+// UPDATE CAMPGROUND ROUTE
+router.put("/campgrounds/:id", middleware.checkCampgroundOwnership, function(req, res){
+    // find and update correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            //redirect to show page
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+
+// DESTROY CAMPGROUND ROUTE
+router.delete("/campgrounds/:id", middleware.checkCampgroundOwnership, function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            console.log(err)
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        };
+    });
+});
+
+module.exports = router;
